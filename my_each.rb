55b3496eff94324bb751ea3e76e7c5ca9ea3dc7b
Renee Sims <info@reneesoul.com>
2019-07-29 03:35:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -11,14 +11,14 @@ def my_each(array)
 
 end
 
-def no_each(array) # creates a method that accepts and argument
-  if block_given? # check t make sure there is a block of code to do the work of the method
-    i=0 # starts a counter to look at each array element
-    while i < array.size # the methods conditions
-      yield(array[i]) # the methods work
-      i += 1 # advances the counter
-    end # ends the methods conditions and counter
-  end # ends the if stetement
-  return array #shows the work results
-end # end the method
+def no_each(array)            # creates a method that accepts and argument
+  if block_given?             # check t make sure there is a block of code to do the work of the method
+    i=0                       # starts a counter to look at each array element
+    while i < array.size      # the methods conditions
+      yield(array[i])         # the methods work
+      i += 1                  # advances the counter
+    end                       # ends the methods conditions and counter
+  end                         # ends the if stetement
+  return array                #shows the work results
+end                           # end the method
     
